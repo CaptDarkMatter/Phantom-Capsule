74b1e889471b17e7155b8fe9e87ee34a762851a1
CaptDarkMatter <reederdt@gmail.com>
2017-12-11 19:48:34 -0600

    First Unity build
    Created the intro video scene and connected it with the main menu scene.

---
+++ Phantom Capsule/Assets/PlayIntroVideo.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class PlayIntroVideo : MonoBehaviour {
+
+    public bool videoFinished;
+
+    public RawImage image;
+    public VideoClip videoToPlay;
+    private VideoPlayer videoPlayer;
+    private VideoSource videoSource;
+    private AudioSource audioSource;
+
+	void Start () {
+        Application.runInBackground = true;
+        StartCoroutine(playVideo());
+    }
+
+    IEnumerator playVideo()
+    {
+        videoPlayer = gameObject.AddComponent<VideoPlayer>();
+        audioSource = gameObject.AddComponent<AudioSource>();
+        videoPlayer.playOnAwake = false;
+        audioSource.playOnAwake = false;
+        audioSource.Pause();
+        videoPlayer.source = VideoSource.VideoClip;
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+        videoPlayer.clip = videoToPlay;
+        videoPlayer.Prepare();
+
+        WaitForSeconds waitTime = new WaitForSeconds(1);
+        while(!videoPlayer.isPrepared)
+        {
+            Debug.Log("preparing video");
+            yield return waitTime;
+            break;
+        }
+        Debug.Log("Done preparing video");
+        image.texture = videoPlayer.texture;
+        videoPlayer.Play();
+        audioSource.Play();
+        Debug.Log("Playing Video");
+        while (videoPlayer.isPlaying)
+        {
+            Debug.LogWarning(" Vidoe time: " + Mathf.FloorToInt((float)videoPlayer.time));
+            yield return null;
+        }
+        Debug.Log("Done playing video");
+        videoFinished = true;
+    }
+	
+	void Update () {
+		
+	}
+}
---
+++ Phantom Capsule/Assets/finishIntro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finishIntro : MonoBehaviour {
+
+    void Start () {
+
+    }
+	
+	void Update () {
+        GameObject introScreen = GameObject.Find("IntroScreen");
+        GameObject isIntroFinished = GameObject.Find("isIntroFinished");
+        PlayIntroVideo playIntroVideo = introScreen.GetComponent<PlayIntroVideo>();
+        ChangeScenes changeScenes = isIntroFinished.GetComponent<ChangeScenes>();
+        if (playIntroVideo.videoFinished == true){
+            changeScenes.SceneToChangeTo = 1;
+            changeScenes.SceneToChange();
+        }
+	}
+}
