e46fd7bcdd4a2bb176e174e5a82f690e2139f449
CaptDarkMatter <reederdt@gmail.com>
2017-12-22 13:14:49 -0600

    Updated turn bar
    Still need to work out transform.position

--- Phantom Capsule/Assets/Scripts/PositionC.cs
+++ Phantom Capsule/Assets/Scripts/PositionC.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PositionC : MonoBehaviour {
 
-    Vector3 pos;
+	public Vector3 pos;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +13,6 @@ public class PositionC : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        pos = transform.position;
+		pos = this.transform.position;
 	}
 }
